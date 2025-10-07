6b0cec5c97053969c92566a9cb7171f262ffdfd3
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2025-10-07 22:02:31 +0500

    feat: add index route handling

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('node:path');
+const indexRouter = require('./router/indexRouter');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use('/', indexRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, (err) => {
