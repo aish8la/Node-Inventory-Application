e091746c617e11abc55e8af137987adf8e8b7c60
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2025-10-09 00:28:25 +0500

    chore: remove logging category data from category get controller

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -2,7 +2,6 @@ const db = require('../db/queries');
 
 async function categoryGet(req, res) {
     const categories = await db.getAllCategories();
-    console.log(categories);
     res.render('category', { title: 'Fab Inventory | Categories', categories: categories});
 }
 
