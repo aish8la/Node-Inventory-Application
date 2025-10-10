66dd3cca0356b456d5dae0c17bc6d1959bc0cad6
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2025-10-10 21:31:00 +0500

    feat: category router explicitly includes validation middleware

--- router/categoryRouter.js
+++ router/categoryRouter.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
 const categoryController = require('../controllers/categoryController');
+const categoryAddEditValidation = require('../validators/categoryFormValidators');
+const { validate } = require('../validators/validationHelpers');
 const categoryRouter = Router();
 
 categoryRouter.get('/', categoryController.categoryGet);
 categoryRouter.get('/new',categoryController.newCategoryGet);
-categoryRouter.post('/new',categoryController.newCategoryPost);
+categoryRouter.post('/new', categoryAddEditValidation, validate, categoryController.newCategoryPost);
 
 module.exports = categoryRouter;
