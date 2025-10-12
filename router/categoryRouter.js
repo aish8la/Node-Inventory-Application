1ab345fdcdd090a74c49ef10b1000aadbf975b36
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2025-10-12 23:50:21 +0500

    fix: add validation middleware to category edit route

--- router/categoryRouter.js
+++ router/categoryRouter.js
@@ -9,7 +9,7 @@ categoryRouter.get('/', categoryController.categoryGet);
 categoryRouter.get('/new',categoryController.newCategoryGet);
 categoryRouter.post('/new', categoryAddEditValidation, validate, categoryController.newCategoryPost);
 categoryRouter.get('/:categoryId/edit', categoryController.editCategoryGet);
-categoryRouter.post('/:categoryId/edit', categoryAddEditValidation, categoryOpAuth, categoryController.editCategoryPost);
+categoryRouter.post('/:categoryId/edit', categoryAddEditValidation, validate, categoryOpAuth, categoryController.editCategoryPost);
 categoryRouter.get('/:categoryId/delete', categoryController.deleteCategoryGet);
 categoryRouter.post('/:categoryId/delete', categoryOpAuth, categoryController.deleteCategoryPost);
 
