065c686c375ac6411cd166e49be73ffd12892cf4
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2025-10-15 22:54:30 +0500

    feat: add route and controller for adding new materials

--- controllers/materialController.js
+++ controllers/materialController.js
@@ -1,4 +1,5 @@
 const db = require('../db/materialQueries');
+const { matchedData } = require('express-validator');
 
 async function materialsGet(req, res) {
     const materials = await db.getAllMaterials();
@@ -20,7 +21,35 @@ async function newMaterialGet(req, res) {
     });
 }
 
+async function newMaterialPost(req, res) {
+    const validatedInput = matchedData(req, {
+        onlyValidData: false,
+        locations: ['body'],
+    });
+    if (res.validationErrors) {
+        const formData = {
+            ...validatedInput,
+            categoryList: await db.getMaterialCategories()
+        }
+        return res.render('material/form', {
+            subtitle: 'Fab Inventory | New Material',
+            mode: 'new',
+            formData,
+            formErrors: res.validationErrors,
+        });
+    }
+    await db.addMaterial({
+        materialName: validatedInput.materialName,
+        materialDescription: validatedInput.materialDescription,
+        stockInHand: validatedInput.stockInHand,
+        isProtected: validatedInput.isProtected,
+        categoriesId: validatedInput.categoriesId,
+    });
+    res.redirect('/material');
+}
+
 module.exports = {
     materialsGet,
     newMaterialGet,
+    newMaterialPost
 };
--- router/materialRouter.js
+++ router/materialRouter.js
@@ -1,8 +1,11 @@
 const { Router } = require('express');
 const materialRouter = Router();
 const materialController = require('../controllers/materialController');
+const materialValidators = require('../validators/materialFormValidators');
+const { validate } = require('../validators/validationHelpers');
 
 materialRouter.get('/', materialController.materialsGet);
 materialRouter.get('/new', materialController.newMaterialGet);
+materialRouter.post('/new', materialValidators.addValidation, validate, materialController.newMaterialPost);
 
 module.exports = materialRouter;
