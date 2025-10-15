0bdf826cf7a33f30167c9337c6e1ca72986b16d0
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2025-10-15 21:37:36 +0500

    feat: add validators for material addition

---
+++ validators/materialFormValidators.js
@@ -0,0 +1,20 @@
+const { body } = require('express-validator');
+
+const addValidation = [
+    body('materialName')
+        .trim()
+        .escape()
+        .notEmpty().withMessage('Material Name cannot be empty')
+        .isLength({min: 1, max: 50}).withMessage('Material name must be between 1 to 50 characters long'),
+    body('materialDescription')
+        .trim()
+        .isInt()
+        .isLength({min: 0, max: 500}).withMessage('Description must not be more than 500 characters long'),
+    body('stockInHand')
+        .trim()
+        .isInt().withMessage('Stock Quantity must be a number'),
+];
+
+module.exports = {
+    addValidation,
+};
