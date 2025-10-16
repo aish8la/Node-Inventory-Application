82492f9ded2c8777fa1a22499bf2157128bc7964
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2025-10-17 04:21:55 +0500

    feat: add an auth middleware function

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,8 @@
+const { matchedData } = require('express-validator');
 const { getCategoryById } = require('../db/categoryQueries');
+const { getMaterialProtectStatus } = require('../db/materialQueries');
 const UnauthorizedError = require('../errors/UnauthorizedError');
+const NotFoundError = require('../errors/NotFoundError');
 const password = process.env.APP_PASSWORD || '';
 
 function isAuthorized(isProtected, req) {
@@ -22,6 +25,18 @@ async function categoryOpAuth(req, res, next) {
     throw new UnauthorizedError("Invalid credentials. Please try again.");
 }
 
+function auth(req, res, next) {
+    const authorized = ({ currentProtectStatus, inputProtectStatus, inputPassword} ) => {
+        if (currentProtectStatus || inputProtectStatus) {
+            return inputPassword === password;
+        } 
+        return true;
+    }
+    req.authorized = authorized;
+    next();
+}
+
 module.exports = {
     categoryOpAuth,
+    auth,
 };
