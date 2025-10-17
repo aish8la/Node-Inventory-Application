a876a8327a108bd6840b4765f04a4a711b05a982
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2025-10-17 20:14:33 +0500

    refactor: remove unused old authentication middleware code

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,29 +1,4 @@
-const { matchedData } = require('express-validator');
-const { getCategoryById } = require('../db/categoryQueries');
-const { getMaterialProtectStatus } = require('../db/materialQueries');
-const UnauthorizedError = require('../errors/UnauthorizedError');
-const NotFoundError = require('../errors/NotFoundError');
 const password = process.env.APP_PASSWORD || '';
-
-function isAuthorized(isProtected, req) {
-    if (!isProtected) {
-        return true;
-    }
-    if (req.body?.password === password) {
-        return true;
-    }
-    return false;
-}
-
-async function categoryOpAuth(req, res, next) {
-    const { categoryId } = req.params;
-    const category = await getCategoryById(categoryId);
-    let isProtected = category[0].is_protected;
-    if (isAuthorized(isProtected, req)) {
-        return next();
-    }
-    throw new UnauthorizedError("Invalid credentials. Please try again.");
-}
 
 function auth(req, res, next) {
     const authorized = ({ currentProtectStatus, inputProtectStatus, inputPassword} ) => {
@@ -37,6 +12,5 @@ function auth(req, res, next) {
 }
 
 module.exports = {
-    categoryOpAuth,
     auth,
 };
