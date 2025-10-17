d8b9c0e4fc5051cbe93de318a4fa45311f9acfd1
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2025-10-17 20:13:13 +0500

    feat: refactor category route to use same auth middleware as material routes

--- router/categoryRouter.js
+++ router/categoryRouter.js
@@ -2,15 +2,15 @@ const { Router } = require('express');
 const categoryController = require('../controllers/categoryController');
 const categoryValidators = require('../validators/categoryFormValidators');
 const { validate } = require('../validators/validationHelpers');
-const { categoryOpAuth } = require('../middleware/auth');
+const { auth } = require('../middleware/auth');
 const categoryRouter = Router();
 
 categoryRouter.get('/', categoryController.categoryGet);
 categoryRouter.get('/new',categoryController.newCategoryGet);
-categoryRouter.post('/new', categoryValidators.addValidation, validate, categoryController.newCategoryPost);
+categoryRouter.post('/new', categoryValidators.addValidation, validate, auth, categoryController.newCategoryPost);
 categoryRouter.get('/:categoryId/edit', categoryValidators.paramValidation, validate, categoryController.editCategoryGet);
-categoryRouter.post('/:categoryId/edit', categoryValidators.editValidation, validate, categoryOpAuth, categoryController.editCategoryPost);
+categoryRouter.post('/:categoryId/edit', categoryValidators.editValidation, validate, auth, categoryController.editCategoryPost);
 categoryRouter.get('/:categoryId/delete', categoryValidators.paramValidation, validate, categoryController.deleteCategoryGet);
-categoryRouter.post('/:categoryId/delete', categoryValidators.paramValidation, validate, categoryOpAuth, categoryController.deleteCategoryPost);
+categoryRouter.post('/:categoryId/delete', categoryValidators.paramValidation, validate, auth, categoryController.deleteCategoryPost);
 
 module.exports = categoryRouter;
