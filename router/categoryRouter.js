1faa4b874814263727ccd9037fcc75b637a05110
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2025-10-17 20:10:53 +0500

    feat: refactor routes to use new category validators

--- router/categoryRouter.js
+++ router/categoryRouter.js
@@ -1,16 +1,16 @@
 const { Router } = require('express');
 const categoryController = require('../controllers/categoryController');
-const categoryAddEditValidation = require('../validators/categoryFormValidators');
+const categoryValidators = require('../validators/categoryFormValidators');
 const { validate } = require('../validators/validationHelpers');
 const { categoryOpAuth } = require('../middleware/auth');
 const categoryRouter = Router();
 
 categoryRouter.get('/', categoryController.categoryGet);
 categoryRouter.get('/new',categoryController.newCategoryGet);
-categoryRouter.post('/new', categoryAddEditValidation, validate, categoryController.newCategoryPost);
-categoryRouter.get('/:categoryId/edit', categoryController.editCategoryGet);
-categoryRouter.post('/:categoryId/edit', categoryAddEditValidation, validate, categoryOpAuth, categoryController.editCategoryPost);
-categoryRouter.get('/:categoryId/delete', categoryController.deleteCategoryGet);
-categoryRouter.post('/:categoryId/delete', categoryOpAuth, categoryController.deleteCategoryPost);
+categoryRouter.post('/new', categoryValidators.addValidation, validate, categoryController.newCategoryPost);
+categoryRouter.get('/:categoryId/edit', categoryValidators.paramValidation, validate, categoryController.editCategoryGet);
+categoryRouter.post('/:categoryId/edit', categoryValidators.editValidation, validate, categoryOpAuth, categoryController.editCategoryPost);
+categoryRouter.get('/:categoryId/delete', categoryValidators.paramValidation, validate, categoryController.deleteCategoryGet);
+categoryRouter.post('/:categoryId/delete', categoryValidators.paramValidation, validate, categoryOpAuth, categoryController.deleteCategoryPost);
 
 module.exports = categoryRouter;
